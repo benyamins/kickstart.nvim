43ff0d2c285d46ec1bf6fe2f471089a195ad733a
benyamins <benjarq@hotmail.com>
2024-08-27 09:17:28 -0400

    En caso de windows ocupar pwsh-preview

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,1 +1,19 @@
 vim.opt.relativenumber = true
+if vim.loop.os_uname().sysname == 'Windows_NT' then
+  -- Check if 'pwsh' (PowerShell Core) is available, otherwise use 'powershell'
+  vim.opt.shell = 'pwsh-preview.cmd'
+
+  -- Configure shellcmdflag
+  vim.opt.shellcmdflag =
+    '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues["Out-File:Encoding"]="utf8";Remove-Alias -Force -ErrorAction SilentlyContinue tee'
+
+  -- Configure shellredir
+  vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+
+  -- Configure shellpipe
+  vim.opt.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+
+  -- Set shellquote and shellxquote to empty strings
+  vim.opt.shellquote = ''
+  vim.opt.shellxquote = ''
+end
